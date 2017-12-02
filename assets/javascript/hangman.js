bd17c52722d610bc57bcdae363d188f24c41228d
anishbnair <nbanish@gmail.com>
2017-12-02 09:52:56 -0500

    added an array to store words

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,15 @@
+
+// create an array with country names
+var wordBank = ['USA', 'INDIA', 'Japan', 'Brazil'];
+console.log(wordBank);
+
+// computer select random country name
+
+
+// user select a letter
+
+//
+
+
+
+
